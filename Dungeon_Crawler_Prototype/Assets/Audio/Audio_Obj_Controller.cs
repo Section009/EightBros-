78944ff19055b0914ce1d1c72ad1a9d6767c182f
RandomGibus <148593977+RandomGibus@users.noreply.github.com>
2025-10-30 12:55:46 -0400

    revamped audio system

--- Dungeon_Crawler_Prototype/Assets/Audio/Audio_Obj_Controller.cs
+++ Dungeon_Crawler_Prototype/Assets/Audio/Audio_Obj_Controller.cs
@@ -5,23 +5,40 @@ using UnityEngine.Audio;
 
 public class Audio_Obj_Controller : MonoBehaviour
 {
-    private AudioSource m_AudioSource;
-    private AudioClip m_AudioClip;
+    public AudioSource[] Sources;
+    private AudioClip[] Clips;
+    public float[] Starts;
+    private bool[] played;
     private float audio_timer;
+    private float audio_timer_max;
     public GameObject nextClip;
     // Start is called before the first frame update
     void Start()
     {
-        m_AudioSource = GetComponent<AudioSource>();
-        m_AudioClip = m_AudioSource.clip;
-        audio_timer = m_AudioClip.length;
+        played = new bool[Sources.Length];
+        for (int i = 0; i < Sources.Length; i++)
+        {
+            float temp = Starts[i] + Sources[i].clip.length;
+            if (temp > audio_timer_max)
+            {
+                audio_timer_max = temp;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        audio_timer -= Time.deltaTime;
-        if (audio_timer < 0)
+        audio_timer += Time.deltaTime;
+        for (int i = 0; i < Sources.Length; i++)
+        {
+            if ((played[i] == false)&&(audio_timer >= Starts[i]))
+            {
+                Sources[i].Play();
+                played[i] = true;
+            }
+        }
+        if (audio_timer >= audio_timer_max)
         {
             if (nextClip != null)
             {
