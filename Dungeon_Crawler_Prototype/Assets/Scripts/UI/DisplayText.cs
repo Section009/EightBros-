344f1212b018c779f489192de0b044f17038e8fc
Section009 <72355643+Section009@users.noreply.github.com>
2025-11-01 14:22:28 -0400

    pushing UI test
    UI Test Scenes and more

---
+++ Dungeon_Crawler_Prototype/Assets/Scripts/UI/DisplayText.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayText : MonoBehaviour
+{
+    public Text textField;
+    // Start is called before the first frame update
+    public GameObject player;
+
+    public Weapon_Projectile weaponProjectile; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (textField != null && weaponProjectile != null)
+        {
+            // Example: Display combo count and cooldown
+            textField.text =
+                "Combo: " + weaponProjectile.combo_count + " Skill Timer: " + weaponProjectile.skill_timer.ToString("F2");
+                //"Combo: " + weaponProjectile.combo_count +
+                //"\nSkill Timer: " + weaponProjectile.skill_timer.ToString("F2");
+        }
+    }
+}
