d45d02213835828e459e18989d7ecf18dd56dc70
RandomGibus <148593977+RandomGibus@users.noreply.github.com>
2025-11-05 13:10:45 -0500

    started work on gong

---
+++ Dungeon_Crawler_Prototype/Assets/Scripts/Movement&Weapons/Gong/Orbiting_Object.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbiting_Object : MonoBehaviour
+{
+    public GameObject target;
+    public float speed;
+    public float lifetime;
+    private float life_timer;
+    public int damage;
+    public float knockback_time;
+    public float knockback_speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(target.transform.position, new Vector3(0, 1, 0), speed * Time.deltaTime);
+        life_timer += Time.deltaTime;
+        if (life_timer >= lifetime)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        Dummy d = col.gameObject.GetComponent<Dummy>();
+        if (d != null)
+        {
+            print("knocked");
+            d.KnockBack(target.transform, knockback_time, knockback_speed);
+        }
+        if (col.gameObject.CompareTag("Enemy"))
+        {
+            print("Kill");
+        }
+    }
+}
