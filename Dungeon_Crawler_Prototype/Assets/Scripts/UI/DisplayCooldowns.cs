d9303f9c872cea7653c912f4c4f82330bd23b818
RandomGibus <148593977+RandomGibus@users.noreply.github.com>
2025-11-19 12:25:36 -0500

    UI now causes change in text colors

--- Dungeon_Crawler_Prototype/Assets/Scripts/UI/DisplayCooldowns.cs
+++ Dungeon_Crawler_Prototype/Assets/Scripts/UI/DisplayCooldowns.cs
@@ -21,26 +21,32 @@ public class DisplayCooldowns : MonoBehaviour
             if (pcm.Melee_Skill_Available)
             {
                 Skill_Timer.text = "Skill Available!(Right Click)";
+                Skill_Timer.color = Color.green;
             }
             else
             {
                 Skill_Timer.text = "Skill Time: " + (pcm.Melee_Skill_Cooldown_Max - pcm.Melee_Skill_Cooldown_timer).ToString("F2");
+                Skill_Timer.color = Color.yellow;
             }
             if (pcm.Melee_Dash_Available)
             {
                 Dash_Timer.text = "Dash Available!(Space)";
+                Dash_Timer.color = Color.green;
             }
             else
             {
                 Dash_Timer.text = "Dash Time: " + (pcm.Melee_Dash_Cooldown_Max - pcm.Melee_Dash_Cooldown_timer).ToString("F2");
+                Dash_Timer.color = Color.yellow;
             }
             if (pcm.Melee_Ultimate_Available)
             {
                 Ultimate_Timer.text = "Ultimate Available!(Center Click)";
+                Ultimate_Timer.color = Color.green;
             }
             else
             {
                 Ultimate_Timer.text = "Ultimate Time: " + (pcm.Melee_Ultimate_Cooldown_Max - pcm.Melee_Ultimate_Cooldown_timer).ToString("F2");
+                Ultimate_Timer.color = Color.yellow;
             }
         }
         
@@ -49,31 +55,38 @@ public class DisplayCooldowns : MonoBehaviour
             if (pcm.Firework_Skill_Available)
             {
                 Skill_Timer.text = "Skill Available!(Right Click)";
+                Skill_Timer.color = Color.green;
             }
             else
             {
                 Skill_Timer.text = "Skill Time: " + (pcm.Firework_Skill_Cooldown_Max - pcm.Firework_Skill_Cooldown_timer).ToString("F2");
+                Skill_Timer.color = Color.yellow;
             }
             if (pcm.Firework_Dash_Available)
             {
                 Dash_Timer.text = "Dash Available!(Space)";
+                Dash_Timer.color = Color.green;
             }
             else
             {
                 Dash_Timer.text = "Dash Time: " + (pcm.Firework_Dash_Cooldown_Max - pcm.Firework_Dash_Cooldown_timer).ToString("F2");
+                Dash_Timer.color = Color.yellow;
             }
             if (pcm.Firework_Ultimate_Available)
             {
                 Ultimate_Timer.text = "Ultimate Available!(Center Click)";
+                Ultimate_Timer.color = Color.green;
             }
             else
             {
                 Ultimate_Timer.text = "Ultimate Time: " + (pcm.Firework_Ultimate_Cooldown_Max - pcm.Firework_Ultimate_Cooldown_timer).ToString("F2");
+                Ultimate_Timer.color = Color.yellow;
             }
         }
 
         if (pcm.Swap_Available)
         {
+            Swap_Timer.color = Color.green;
             if (pcm.Melee_Open)
             {
                 Swap_Timer.text = "Swap to Fireworks with E!";
@@ -86,6 +99,7 @@ public class DisplayCooldowns : MonoBehaviour
         else
         {
             Swap_Timer.text = "Time to Next Swap: " + (pcm.Swap_Cooldown_Max - pcm.Swap_Cooldown_timer).ToString("F2");
+            Swap_Timer.color = Color.yellow;
         }
     }
 }
