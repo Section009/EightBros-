92b28c2607c1016521e6120adabf73ce17965f14
RandomGibus <148593977+RandomGibus@users.noreply.github.com>
2025-11-25 14:41:36 -0500

    Screen_shake ready for implementation

--- Dungeon_Crawler_Prototype/Assets/Scripts/Movement&Weapons/Camera_Follow.cs
+++ Dungeon_Crawler_Prototype/Assets/Scripts/Movement&Weapons/Camera_Follow.cs
@@ -7,7 +7,20 @@ public class Camera_Follow : MonoBehaviour
     public GameObject follow;
     public Vector3 offset = new Vector3(0f, 10f, -8.5f);
     public bool active = true;
-
+    public float shake_intensity = 1.0f;
+    public bool camera_shaking = false;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            camera_shaking = true;
+        }
+        if (camera_shaking)
+        {
+            StartCoroutine(CameraShakeRoutine());
+        }
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -15,5 +28,12 @@ public class Camera_Follow : MonoBehaviour
         {
             transform.position = follow.transform.position + offset;
         }
+    }
+    IEnumerator CameraShakeRoutine()
+    {
+        transform.position = follow.transform.position + offset + (Random.insideUnitSphere * shake_intensity);
+        yield return new WaitForSeconds(0.25f);
+        transform.position = follow.transform.position + offset;
+        camera_shaking = false;
     }
 }
