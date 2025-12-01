65e2a4fa3d1bae0250ec2189c208bec8d8cb2146
RandomGibus <148593977+RandomGibus@users.noreply.github.com>
2025-11-30 22:50:54 -0500

    finished dialogue system

--- Dungeon_Crawler_Prototype/Assets/Scripts/UI/Dialogue_Master.cs
+++ Dungeon_Crawler_Prototype/Assets/Scripts/UI/Dialogue_Master.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Dialogue_Master : MonoBehaviour
 {
     public string[] dialogue;
+    public string[] speaker;
+    public Text Display_Name;
     public Text Display_Text;
     private int cur_num = 0;
     public string NextLevel;
+    public UnityEvent[] Dialogue_Events;
     // Start is called before the first frame update
     void Start()
     {
         Display_Text.text = dialogue[0];
+        Display_Name.text = speaker[0];
+        Dialogue_Events[0]?.Invoke();
     }
 
     // Update is called once per frame
@@ -27,6 +33,8 @@ public class Dialogue_Master : MonoBehaviour
         else
         {
             Display_Text.text = dialogue[cur_num];
+            Display_Name.text = speaker[cur_num];
+            Dialogue_Events[cur_num]?.Invoke();
         }
     }
 }
