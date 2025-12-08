952f5e375a9be10339934e7e6072684579a94bb4
RandomGibus <148593977+RandomGibus@users.noreply.github.com>
2025-12-08 00:45:51 -0500

    Idle animation now functional

--- Dungeon_Crawler_Prototype/Assets/Scripts/Movement&Weapons/Player_Movement.cs
+++ Dungeon_Crawler_Prototype/Assets/Scripts/Movement&Weapons/Player_Movement.cs
@@ -15,13 +15,21 @@ public class Player_Movement : MonoBehaviour
     private float Dash_Timer;
     public bool Dashing;
     public bool Locked;
+    private bool moving;
     public Rigidbody rb;
     public bool slowed;
     public float slow_reduction;
     public string RunName;
     public string IdleName;
     public string IdleAnimName;
+    public float IdleAnimLength;
+    private bool Idling = false;
+    private float IdleAnimation_Timer = 0;
+    public float IdleCooldown;
     public string HitName;
+    public float HitAnimLength;
+    private bool hit = false;
+    private float hit_timer = 0;
     private Health health_hub;
 
     private Animator animator;
@@ -55,7 +63,7 @@ public class Player_Movement : MonoBehaviour
                 transform.LookAt(transform.position + vec, Vector3.up);
                 if (vec.magnitude != 0)
                 {
-                    animator.Play(RunName);
+                    moving = true;
                     rb.velocity = transform.forward * Speed;
                     if (slowed)
                     {
@@ -65,8 +73,57 @@ public class Player_Movement : MonoBehaviour
 
                 else
                 {
+                    moving = false;
                     rb.velocity = new Vector3(0f, 0f, 0f);
-                    animator.Play(IdleName);
+                }
+            }
+            else
+            {
+                Idling = false;
+                IdleAnimation_Timer = 0f;
+            }
+            if (hit)
+            {
+                Idling = false;
+                IdleAnimation_Timer = 0f;
+                animator.Play(HitName);
+                hit_timer += Time.deltaTime;
+                if (hit_timer >= HitAnimLength)
+                {
+                    hit_timer = 0f;
+                    hit = false;
+                }
+            }
+            else
+            {
+                if (moving)
+                {
+                    animator.Play(RunName);
+                    Idling = false;
+                    IdleAnimation_Timer = 0f;
+                }
+                else
+                {
+                    if (Idling)
+                    {
+                        animator.Play(IdleAnimName);
+                        IdleAnimation_Timer += Time.deltaTime;
+                        if (IdleAnimation_Timer >= IdleAnimLength)
+                        {
+                            IdleAnimation_Timer = 0f;
+                            Idling = false;
+                        }
+                    }
+                    else
+                    {
+                        animator.Play(IdleName);
+                        IdleAnimation_Timer += Time.deltaTime;
+                        if (IdleAnimation_Timer >= IdleCooldown)
+                        {
+                            IdleAnimation_Timer = 0f;
+                            Idling = true;
+                        }
+                    }
                 }
             }
         }
@@ -77,7 +134,7 @@ public class Player_Movement : MonoBehaviour
     {
         if (Input.GetKeyDown("q"))
         {
-            Damage_Player(100.0f);
+            Damage_Player(5.0f);
         }
 
         if (Input.GetKeyDown("t"))
@@ -109,6 +166,7 @@ public class Player_Movement : MonoBehaviour
 
     public void Damage_Player(float damage)
     {
+        hit = true;
         health_hub.TakeDamage(Mathf.RoundToInt(damage));
     }
 
