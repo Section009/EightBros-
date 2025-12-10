29a18a0ddc826b128985377312169c8e4f791d36
trist246 <43695817+trist246@users.noreply.github.com>
2025-12-09 19:37:05 -0500

    fading music

--- Dungeon_Crawler_Prototype/Assets/Audio/Music/Music_Master.cs
+++ Dungeon_Crawler_Prototype/Assets/Audio/Music/Music_Master.cs
@@ -6,20 +6,28 @@ using UnityEngine.Audio;
 public class Music_Master : MonoBehaviour
 {
     public AudioClip Idle_Track, Battle_Track;
-    AudioSource audioSource;
+    public AudioSource source1;
+    public AudioSource source2;
+    public float Duration = 1f;
+
+
+    AudioSource curSource;
+    AudioSource nextSource;
     bool fighting = false;
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        curSource = source1;
+        nextSource = source2;
+        nextSource.volume = 0f;
     }
 
     public void Enter_Battle()
     {
         if (fighting == false)
         {
-            audioSource.clip = Battle_Track;
-            audioSource.Play();
+            StopAllCoroutines();
+            StartCoroutine(Fade_In());
             fighting = true;
         }
     }
@@ -28,9 +36,34 @@ public class Music_Master : MonoBehaviour
     {
         if (fighting == true) 
         {
-            audioSource.clip = Idle_Track;
-            audioSource.Play();
+            StopAllCoroutines();
+            StartCoroutine(Fade_In());
             fighting = false;
         }
+    }
+
+    IEnumerator Fade_In()
+    {
+        nextSource.Play();
+
+        float timer = 0f;
+        while (timer < Duration)
+        {
+            timer += Time.deltaTime;
+            float normalTime = timer / Duration;
+
+            curSource.volume = Mathf.Lerp(1f, 0f, normalTime);
+            nextSource.volume = Mathf.Lerp(0f, 1f, normalTime);
+
+            yield return null;
+        }
+
+        curSource.volume = 0f;
+        nextSource.volume = 1f;
+
+        curSource.Stop();
+        AudioSource tmp = curSource;
+        curSource = nextSource;
+        nextSource = tmp;
     }
 }
